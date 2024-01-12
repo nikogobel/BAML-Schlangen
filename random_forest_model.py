00c0c5f8d766596477606f3ec4a1265c3e766ff5
Nikolai Göbel <niko.goebel@tum.de>
2024-01-12 16:43:21 +0100

    Add random forest model for classification

---
+++ random_forest_model.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+import matplotlib.pyplot as plt
+
+df = pd.read_pickle('cleaned_dataset.pkl')
+
+#print(df.info())
+#print(df.head())
+
+# Identify non-numeric columns
+non_numeric_cols = df.select_dtypes(include=['object', 'category']).columns
+
+# Apply one-hot encoding to non-numeric columns
+df = pd.get_dummies(df, columns=non_numeric_cols, drop_first=True)
+df = df.dropna()
+
+train_model = RandomForestClassifier(n_estimators=5, max_features=3, random_state=2023+2024)
+
+
+X = df.drop(columns=['reviews_Like_True'])
+y = df['reviews_Like_True']
+
+train_model.fit(X,y)
+print("done fitting")
+
+pred = train_model.predict(X)
+error_rate = np.mean(y != pred)
+print("Error rate:", error_rate)
+print("Accuracy:", accuracy_score(y, pred))
